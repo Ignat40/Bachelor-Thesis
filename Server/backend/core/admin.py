d9a5ba161ccc72a9542d487fa57b81a67710a5e9
Ignat Bojinov <ignatbojinov@gmail.com>
2026-04-21 17:52:25 +0200

    New Structure

--- Server/backend/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Patient, Exercise, Session, Attempt
-
-admin.site.register(Patient)
-admin.site.register(Exercise)
-admin.site.register(Session)
-admin.site.register(Attempt)
